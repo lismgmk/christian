c24318e16b9e1249ff63fb9682f0c5f4902a690b
systems4express <[lismgmk2@gmail.com]>
2022-01-28 14:32:25 +0300

    little fix

--- pages/index.vue
+++ pages/index.vue
@@ -7,12 +7,22 @@
 </template>
 
 <script>
+import Header from '/components/Header.vue';
+import TitleNote from '/components/TitleNote.vue';
+import Content from '/components/Content.vue';
 
 export default {
-  name: 'NuxtTutorial',
 
+  name: 'NuxtTutorial',
+  components: {
+    Header,
+    TitleNote,
+    Content
+  },
   mounted() {
     if (process.browser){
+      console.log(this.$i18n.locale, 'locale')
+      console.log(window.navigator.language, 'win')
       this.$i18n.locale = window.navigator.language
       // this.$router.push({ path:  this.switchLocalePath(this.$i18n.locale.slice(0,2))})
     }
